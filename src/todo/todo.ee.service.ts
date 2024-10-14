67ad61cb50e2c6bb64e0d1dd1f6208a645711f1c
Akshay Sasidharan <akshaysasidharan93@gmail.com>
2024-10-14 23:00:15 +0530

    initial ee server

---
+++ src/todo/todo.ee.service.ts
@@ -0,0 +1,13 @@
+import { ITodoService } from '../../../../ce/server/src/todo/types';
+import { Todo } from '../../../../ce/server/src/todo/todo.entity'
+import { Repository } from 'typeorm';
+
+// Override only the create method and leave the rest
+  export default class TodoEEService implements Partial<ITodoService> {
+  async create(todoRepository: Repository<Todo>, title: string): Promise<Todo> {
+    const todo = new Todo();
+    todo.title = title;
+    return todoRepository.save(todo);
+  }
+  // Other methods will fall back to base implementation
+};
