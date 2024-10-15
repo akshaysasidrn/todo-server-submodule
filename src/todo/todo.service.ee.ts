b0540b2e16d69546d8aca12fb978f032f50562e5
Akshay Sasidharan <akshaysasidharan93@gmail.com>
2024-10-15 20:07:04 +0530

    custom ee title

--- src/todo/todo.service.ee.ts
+++ src/todo/todo.service.ee.ts
@@ -1,12 +1,14 @@
 import TodoServiceCe from '@ce/todo/todo.service.ce';
 import { Todo } from '@ce/todo/todo.entity';
 import { Repository } from 'typeorm';
+import { isEmpty, kebabCase } from 'lodash';
 
 // Override only the create method and leave the rest
 export default class TodoServiceEe extends TodoServiceCe {
   async create(todoRepository: Repository<Todo>, title: string): Promise<Todo> {
     console.log('TodoServiceEe create method called');
     const todo = new Todo();
+    isEmpty(title) ? (title = kebabCase('New Todo')) : title;
     todo.title = `[EE Edition] ${title}`;
     return todoRepository.save(todo);
   }
