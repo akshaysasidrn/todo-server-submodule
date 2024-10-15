78715dde7be213ccac306e9eeee0866c821de277
Akshay Sasidharan <akshaysasidharan93@gmail.com>
2024-10-15 19:44:13 +0530

    revise service

--- src/todo/todo.service.ee.ts
+++ src/todo/todo.service.ee.ts
@@ -1,12 +1,13 @@
-import { ITodoService } from '@ce/todo/types';
+import TodoServiceCe from '@ce/todo/todo.service.ce';
 import { Todo } from '@ce/todo/todo.entity';
 import { Repository } from 'typeorm';
 
 // Override only the create method and leave the rest
-export default class TodoEEService implements Partial<ITodoService> {
+export default class TodoServiceEe extends TodoServiceCe {
   async create(todoRepository: Repository<Todo>, title: string): Promise<Todo> {
+    console.log('TodoServiceEe create method called');
     const todo = new Todo();
-    todo.title = title;
+    todo.title = `[EE Edition] ${title}`;
     return todoRepository.save(todo);
   }
   // Other methods will fall back to base implementation
