268280666f5ce59f0bdbb4e233f31e69c67ee929
Akshay Sasidharan <akshaysasidharan93@gmail.com>
2024-10-17 21:20:59 +0530

    override update todo behaviour

--- src/todo/todo.service.ee.ts
+++ src/todo/todo.service.ee.ts
@@ -8,8 +8,23 @@ export default class TodoServiceEe extends TodoServiceCe {
   async create(todoRepository: Repository<Todo>, title: string): Promise<Todo> {
     console.log('TodoServiceEe create method called');
     const todo = new Todo();
-    isEmpty(title) ? (title = kebabCase('New Todo')) : title;
+    title = isEmpty(title) ? kebabCase('New Todo') : title;
     todo.title = `[EE Edition] ${title}`;
+    return todoRepository.save(todo);
+  }
+
+  async update(
+    todoRepository: Repository<Todo>,
+    id: number,
+    isCompleted: boolean,
+    title: string,
+  ): Promise<Todo> {
+    const todo = await todoRepository.findOne({ where: { id } });
+    if (!todo) throw new Error(`Todo with id ${id} not found`);
+    todo.isCompleted = isCompleted;
+    title = isEmpty(title) ? kebabCase('Edited Todo') : title;
+    todo.title = `[EE Edition] ${title}`;
+
     return todoRepository.save(todo);
   }
   // Other methods will fall back to base implementation
